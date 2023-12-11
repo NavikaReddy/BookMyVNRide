2206a22c1c0c0c55413da22992d033d5118d2adf
NavikaReddy <navikareddy12@gmail.com>
2023-12-11 10:59:10 +0530

    pass download

--- BookMyVNRide-DBMS/Register.aspx.cs
+++ BookMyVNRide-DBMS/Register.aspx.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
-using System.Web;
+using System.Data.SqlClient;
+using System.IO;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using Microsoft.Ajax.Utilities;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+
+
 
 namespace BookMyVNRide_DBMS
 {
@@ -17,7 +19,7 @@ namespace BookMyVNRide_DBMS
         {
             if (!IsPostBack)
             {
-                    getRouteNo();
+                getRouteNo();
             }
         }
 
@@ -75,7 +77,7 @@ namespace BookMyVNRide_DBMS
             {
                 con.Open();
                 SqlCommand cmd = new SqlCommand("sp_insert", con);
-                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@sname", txtname.Text);
                 cmd.Parameters.AddWithValue("@rollno", txtroll.Text);
                 cmd.Parameters.AddWithValue("@branch", txtbranch.Text);
@@ -84,15 +86,19 @@ namespace BookMyVNRide_DBMS
                 cmd.Parameters.AddWithValue("@boardingPoint", ddlboardingpt.SelectedItem.Value);
                 cmd.Parameters.AddWithValue("@phone", txtmobile.Text);
                 int x = cmd.ExecuteNonQuery();
+
                 if (x > 0)
                 {
-                    ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('Get your bus pass at college');", true);
+                    // Generate and download PDF
+                    GenerateAndDownloadPDF(txtname.Text, txtroll.Text, txtbranch.Text, ddlroute.SelectedItem.Value, ddlyear.SelectedItem.Value, ddlboardingpt.SelectedItem.Value, txtmobile.Text);
+
+                    // Display a message to the user
+                    ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('Bus pass application submitted. PDF has been generated and downloaded.');", true);
                 }
                 else
                 {
                     Response.Write("Something went wrong");
                 }
-
             }
             catch (Exception ex)
             {
@@ -100,5 +106,44 @@ namespace BookMyVNRide_DBMS
             }
             finally { con.Close(); }
         }
+
+        private void GenerateAndDownloadPDF(string name, string roll, string branch, string route, string year, string boardingPoint, string phone)
+        {
+            //Create a new PDF document.
+            PdfDocument document = new PdfDocument();
+
+            //Add a page to the document.
+            PdfPage page = document.Pages.Add();
+
+            //Create PDF graphics for the page.
+            PdfGraphics graphics = page.Graphics;
+
+            //Set the standard font.
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+
+            //Draw the text.
+            graphics.DrawString("Name: " + name + "\nRoll no: " + roll + " Branch: "+ branch + "\nRoute: " + route + " Boarding point: " + boardingPoint , font, PdfBrushes.Black, new PointF(0, 0));
+
+            //Saving the PDF to the MemoryStream.
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream);
+
+            //Set the position as '0'.
+            stream.Position = 0;
+
+            //Defining the ContentType for pdf file.
+            string contentType = "application/pdf";
+
+            //Define the file name.
+            string fileName = "BusPass.pdf";
+
+            Response.Clear();
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", $"attachment; filename={fileName}");
+            Response.BinaryWrite(stream.ToArray());
+            Response.End();
+
+        }
+
     }
 }
